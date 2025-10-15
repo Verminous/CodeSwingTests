dc9b29dcb7238feaea630176bcfda404cc4a0732
Verminous <developer+goat_github@slmail.me>
2025-10-15 22:48:09 +0200

    1

--- script_tests.js
+++ script_tests.js
@@ -12,6 +12,16 @@ for (let i = 1; i <= 100; i++){
     }
 }
 
+let i = 1;
+while (i <= 100) {
+  let output = '';
+  if (i % 3 === 0) output += 'Fizz';
+  if (i % 5 === 0) output += 'Buzz';
+  console.log(output || i);
+  i++;
+}
+
+
 for (let i = 1; i <= 100; i++){
     let out = '';
     if (i % 3 === 0) out += 'Fizz';
